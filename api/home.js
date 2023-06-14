0655902f1367e537cad8928a3f99766adabc4b08
ZiLinling <2160788947@qq.com>
2023-06-14 10:01:24 +0800

    类别页面样式更新

--- api/home.js
+++ api/home.js
@@ -6,7 +6,7 @@ export function page(pageNum,pageSize,breedName,specie) {
 	return postJsonRequest('/pet/page', {pageNum,pageSize,breedName,specie})
 }
 export function getCount(id) {
-	return postRequest('/pet/getCount',{id})
+	return getRequest('/pet/getCount',{id})
 }
 export function getCountBySpecie(specie) {
 	return getRequest('/pet/getCountBySpecie',{specie})
